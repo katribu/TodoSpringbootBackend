aae1f4d7881ec81602f580030a44b619888b53db
Kat <katrinaburwash_17@hotmail.com>
2024-01-26 14:23:48 +0100

    First Commit

---
+++ src/main/java/com/kat/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.kat.repository;
+import com.kat.model.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+
+
+public interface TaskRepository extends MongoRepository<Task, String> {
+    List<Task> findBySeverity(int severity);
+    @Query("{assignee: ?0}")
+    List<Task> getTasksByAssignee(String assignee);
+}
---
+++ src/test/java/com/kat/Springboottutorial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboottutorial2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
